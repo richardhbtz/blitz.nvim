f0a8aa765f3bc474c1a21dec15e0daf9d0225436
richardhbtz <richardhabitzreuter@icloud.com>
2024-07-01 09:04:39 -0300

    Added a keybind for closing buffers

--- lua/blitz/keys.lua
+++ lua/blitz/keys.lua
@@ -4,6 +4,33 @@ local key = vim.keymap
 local terminal = require("nvterm.terminal")
 local builtin = require("telescope.builtin")
 
+local function closebuffer()
+	if vim.bo.buftype == "terminal" then
+		vim.cmd(vim.bo.buflisted and "set nobl | enew" or "hide")
+	else
+		bufnr = vim.api.nvim_get_current_buf()
+		local curBufIndex = require("bufferline.utils").getBufIndex(bufnr)
+		local bufhidden = vim.bo.bufhidden
+		if bufhidden == "wipe" then
+			vim.cmd("bw")
+			return
+		elseif curBufIndex and #vim.t.bufs > 1 then
+			local newBufIndex = curBufIndex == #vim.t.bufs and -1 or 1
+			vim.cmd("b" .. vim.t.bufs[curBufIndex + newBufIndex])
+		elseif not vim.bo.buflisted then
+			vim.cmd("b" .. vim.t.bufs[1] .. " | bw" .. bufnr)
+			return
+		else
+			vim.cmd("enew")
+		end
+
+		if not (bufhidden == "delete") then
+			vim.cmd("confirm bd" .. bufnr)
+		end
+	end
+	vim.cmd("redrawtabline")
+end
+
 vim.g.mapleader = cfg.leader_key
 
 key.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -18,6 +45,9 @@ end)
 key.set({ "n", "t" }, "<A-v>", function()
 	terminal.toggle("vertical")
 end)
+
+-- themes
+key.set("n", "<Leader>x", closebuffer)
 
 -- themes
 key.set("n", "<Leader>th", ":Telescope themes<CR>", { noremap = true, silent = true, desc = "Theme Switcher" })
